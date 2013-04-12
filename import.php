b5f5c24a9deabc23ac5a0317b326d290a6102c1f
Dylan Corwin <dylandcor@gmail.com>
2013-04-11 22:04:34 -0400

    import not working, but code is there

--- import.php
+++ import.php
@@ -2,7 +2,7 @@
 ob_start();
 
 include('includes/sc-includes.php');
-$pagetitle = 'Contact';
+$pagetitle = 'Import Spreadsheet';
 
 if (!empty($_GET['csv']) && $_GET['csv'] == 'import') { 
 
@@ -11,110 +11,99 @@ $handle = fopen ($_FILES['csv']['tmp_name'],"r");
 
 	$cf = array();
 
-while ($data = fgetcsv($handle, 1000, ",")) {
+while ($data = fgetcsv($handle, 0, ",")) {
 
-//custom field array
-if ($row == 1) {
-	foreach ($data as $key => $value) {
-		if ($key > 14) {
-			$cf[$key] = $value; 
-		}
-	}
-}
-//
+    //custom field array
+    if ($row == 1) {
+    	foreach ($data as $key => $value) {
+    		if ($key > 18) {
+    			$cf[$key] = $value; 
+    		}
+    	}
+    }
+    //
+    
+    //end add extra fields
+    
+    // $checkc = mysql_num_rows(mysql_query("SELECT * FROM contacts WHERE contact_id = ".$data[0].""));
+    
+    // if ($checkc > 0) {
+    
+    //     mysql_query("UPDATE contacts SET
+        
+    //     	contact_first = '".addslashes($data[1])."',
+    //     	contact_last = '".addslashes($data[2])."',
+    //     	contact_title = '".addslashes($data[3])."',
+    //     	contact_company = '".addslashes($data[4])."',
+    //     	contact_street = '".addslashes($data[5])."',
+    //     	contact_city = '".addslashes($data[6])."',
+    //     	contact_state = '".addslashes($data[7])."',
+    //     	contact_zip = '".addslashes($data[8])."',
+    //     	contact_country = '".addslashes($data[9])."',
+    //     	contact_email = '".addslashes($data[10])."',
+    //     	contact_phone = '".addslashes($data[11])."',
+    //     	contact_cell = '".addslashes($data[12])."',
+    //     	contact_web = '".addslashes($data[13])."',
+    //     	contact_profile = '".addslashes($data[14])."'
+        
+    //     WHERE contact_id = ".$data['0']."
+    //     ");
+    
+    // }
+    //
+    
+// else { 
 
-//end add extra fields
-
-$checkc = mysql_num_rows(mysql_query("SELECT * FROM contacts WHERE contact_id = ".$data[0].""));
-
-	if ($checkc > 0) {
-
-mysql_query("UPDATE contacts SET
-
-	contact_first = '".addslashes($data[1])."',
-	contact_last = '".addslashes($data[2])."',
-	contact_title = '".addslashes($data[3])."',
-	contact_company = '".addslashes($data[4])."',
-	contact_street = '".addslashes($data[5])."',
-	contact_city = '".addslashes($data[6])."',
-	contact_state = '".addslashes($data[7])."',
-	contact_zip = '".addslashes($data[8])."',
-	contact_country = '".addslashes($data[9])."',
-	contact_email = '".addslashes($data[10])."',
-	contact_phone = '".addslashes($data[11])."',
-	contact_cell = '".addslashes($data[12])."',
-	contact_web = '".addslashes($data[13])."',
-	contact_profile = '".addslashes($data[14])."'
-
-WHERE contact_id = ".$data['0']."
-");
-
-}
-//
-
-else { 
-
-
-if ($row > 1) {
-
+echo "JKLFDAJKLFADSJKLFDASJKLFDSJKLFDASJKLFDASJKLFDASJKLFDASJKLFDASJKLFDASJKLFDASJKLFDSAJKL";
 //INSERT NEW RECORDS
-mysql_query("INSERT INTO contacts (contact_first, contact_last, contact_title, contact_company, contact_street, contact_city, contact_state, contact_zip, contact_country, contact_email, contact_phone, contact_fax, contact_web, contact_profile) VALUES
-
-(
-	   '".addslashes($data[1])."',
-	   '".addslashes($data[2])."',
-	   '".addslashes($data[3])."',
-	   '".addslashes($data[4])."',
-	   '".addslashes($data[5])."',
-	   '".addslashes($data[6])."',
-	   '".addslashes($data[7])."',
-	   '".addslashes($data[8])."',
-	   '".addslashes($data[9])."',
-	   '".addslashes($data[10])."',
-	   '".addslashes($data[11])."',
-	   '".addslashes($data[12])."',
-	   '".addslashes($data[13])."',
-	   '".addslashes($data[14])."'
-)
-
-");
+    mysql_query("INSERT INTO  donors ('id', 'preferred_mail_name', 'preffered_name_sort', 'preffered_address', 'preferred_city', 'preferred_state_code', 'preferred_country', 'email')
+    VALUES
+    (
+    	   '".addslashes($data[1])."',
+    	   '".addslashes($data[2])."',
+    	   '".addslashes($data[3])."',
+    	   '".addslashes($data[13])."',
+    	   '".addslashes($data[14])."',
+    	   '".addslashes($data[15])."',
+    	   '".addslashes($data[16])."'
+    )
+    ");
 
 	$cid = mysql_insert_id();
+    echo $cid;
 
-//add extra fields
-foreach ($cf as $key => $value) {
+// //add extra fields
+// foreach ($cf as $key => $value) {
 
-record_set('fields',"SELECT * FROM fields WHERE field_title = '".addslashes($value)."'");
+// record_set('fields',"SELECT * FROM fields WHERE field_title = '".addslashes($value)."'");
 
-	if ($totalRows_fields) {
-		mysql_query("INSERT INTO fields_assoc (cfield_field, cfield_contact, cfield_value) VALUES
+// 	if ($totalRows_fields) {
+// 		mysql_query("INSERT INTO fields_assoc (cfield_field, cfield_contact, cfield_value) VALUES
 			
-			(
-				'".$row_fields['field_id']."',
-				'".$cid."',
-				'".addslashes($data[$key])."'
-			)
+// 			(
+// 				'".$row_fields['field_id']."',
+// 				'".$cid."',
+// 				'".addslashes($data[$key])."'
+// 			)
 		
-		");
-	}
+// 		");
+// 	}
 
-}
+// }
 //end add extra fields
 
-mysql_query("INSERT INTO history (history_contact, history_date, history_status) VALUES
-(
-	".$cid.",
-	".time().",
-	1
-)
-");
+// mysql_query("INSERT INTO history (history_contact, history_date, history_status) VALUES
+// (
+// 	".$cid.",
+// 	".time().",
+// 	1
+// )
+// ");
 
 //
-}
-$row++;
+    $row++;
 }
 
-}
 
 
 header('Location: contacts.php');
@@ -125,7 +114,7 @@ header('Location: contacts.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title><?php echo $pagetitle; ?>s</title>
+<title><?php echo $pagetitle; ?></title>
 <script src="includes/lib/prototype.js" type="text/javascript"></script>
 <script src="includes/src/effects.js" type="text/javascript"></script>
 <script src="includes/validation.js" type="text/javascript"></script>
