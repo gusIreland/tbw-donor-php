ab9da9c81151c805dc7271a36ef21b3ffa4322f4
Dylan Corwin <dylandcor@gmail.com>
2013-05-10 12:34:11 -0400

    comments and formatting

--- donations.php
+++ donations.php
@@ -50,10 +50,6 @@
     //datePicker
     
     
-    
-    
-    
-    
     //PAGINATION
     $limit = "";
     $epp = 25;  //entries per page
@@ -73,88 +69,78 @@
     $limit = "LIMIT $offset, $entries_per_page";
     //
     
-    //get donations for the current page with the current sort
-    record_set('contactlist',"SELECT * FROM donations, contacts WHERE donations.donor_id = contacts.contact_id $sorder $limit ");
-
-    // record_set('donors', "SELECT * FROM contacts");
-
-    // $donors_array = array();
-
-    // $i = 1; do {  
-    //       $donors_array[$i] = $row_donors['contact_first']; 
-    // $i++; } while ($row_donors = mysql_fetch_assoc($donors)); 
+    //get donations and their donors for the current page with the current sort
+    record_set('contactlist',"SELECT * FROM donations, contacts WHERE donations.donor_id = contacts.contact_id $sorder $limit");
  ?>
  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
  <html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <title><?php echo $pagetitle; ?>s</title>
-    <script src="includes/lib/prototype.js" type="text/javascript"></script>
-    <script src="includes/src/effects.js" type="text/javascript"></script>
-    <script src="includes/validation.js" type="text/javascript"></script>
-    <script src="includes/src/scriptaculous.js" type="text/javascript"></script>
-
-    <link href="includes/style.css" rel="stylesheet" type="text/css" />
-    <link href="includes/simplecustomer.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-    <?php include('includes/header.php'); ?>
-  
-    <div class="container">
-        <div class="leftcolumn">
-            <h2>Donations</h2>
-            <?php if (!$totalRows_contactlist) { ?>
-                <br />
-                No donations have been added yet.
-                <br />
-                <br />
-                <strong><a href="contact.php">Add</a> or <a href="import.php">Import</a> Contacts </strong><br />
-                <br />
-            <?php } ?>
-            
-            <?php if ($totalRows_contactlist) { ?>
-                <form id="form1" name="form1" method="post" action="">
-                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                        <tr>
-                            <td colspan="4" align="right">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="4"><?php display_msg(); ?></td>
-                        </tr>
-                        <tr>
-                            <th width="26%"  style="padding-left:5px"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $name; ?>">Donor Name</a></th>
-                            <th width="27%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $campaign; ?>">Campaign </a></th>
-                            <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $amount; ?>">Donation Amount</a></th>
-                            <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $date; ?>">Date of Donation</a></th>
-                            <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $matching_company; ?>">Matching Company</a></th>
-                    
-                            <th width="7%">&nbsp;</th>
-                        </tr>
-                    
-                        <?php $row_count = 1; do {  ?>
-                            <tr <?php if ($row_count%2) { ?>bgcolor="#F4F4F4"<?php } ?>>
-                                <td style="padding-left:5px">
-                                    <a href="contact-details.php?id=<?php echo $row_contactlist['donor_id']; ?>"><?php echo $row_contactlist['contact_first']; ?> <?php echo $row_contactlist['contact_last']; ?></a></td>
-                                <td><?php echo $row_contactlist['receipt_number'] ? $row_contactlist['alloc_short_name'] : $na; ?></td>
-                                <td><?php echo $row_contactlist['legal_amount'];?></td>
-                                <td><?php echo $row_contactlist['date_added']; ?></td>
-                                <td><?php echo $row_contactlist['match_company_name']; ?></td>
-                                <td><a href="delete.php?donation=<?php echo $row_contactlist['id']; ?>" onclick="javascript:return confirm('Are you sure?')">Delete</a></td>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <title><?php echo $pagetitle; ?>s</title>
+        <script src="includes/lib/prototype.js" type="text/javascript"></script>
+        <script src="includes/src/effects.js" type="text/javascript"></script>
+        <script src="includes/validation.js" type="text/javascript"></script>
+        <script src="includes/src/scriptaculous.js" type="text/javascript"></script>
+    
+        <link href="includes/style.css" rel="stylesheet" type="text/css" />
+        <link href="includes/simplecustomer.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+        <?php include('includes/header.php'); ?>
+      
+        <div class="container">
+            <div class="leftcolumn">
+                <h2>Donations</h2>
+                <?php if (!$totalRows_contactlist) { ?>
+                    <br />
+                    No donations have been added yet.
+                    <br />
+                    <br />
+                    <strong><a href="contact.php">Add</a> or <a href="import.php">Import</a> Contacts </strong><br />
+                    <br />
+                <?php } ?>
+                
+                <?php if ($totalRows_contactlist) { ?>
+                    <form id="form1" name="form1" method="post" action="">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td colspan="4" align="right">
+                                </td>
                             </tr>
-                        <?php $row_count++; } while ($row_contactlist = mysql_fetch_assoc($contactlist)); ?>
-                    </table>
-                </form>
-                <?php include('includes/pagination_contacts.php'); ?>
-            <?php } ?>
-            
-</div>
-<?php include('includes/right-column.php'); ?>
-<br clear="all" />
-</div>
-
-<?php include('includes/footer.php'); ?>
-
-</body>
+                            <tr>
+                                <td colspan="4"><?php display_msg(); ?></td>
+                            </tr>
+                            <tr>
+                                <th width="26%"  style="padding-left:5px"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $name; ?>">Donor Name</a></th>
+                                <th width="27%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $campaign; ?>">Campaign </a></th>
+                                <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $amount; ?>">Donation Amount</a></th>
+                                <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $date; ?>">Date of Donation</a></th>
+                                <th width="40%"><a href="?page=<?php echo $page_number; ?>&amp;<?php echo $matching_company; ?>">Matching Company</a></th>
+                        
+                                <th width="7%">&nbsp;</th>
+                            </tr>
+                        
+                            <?php $row_count = 1; do {  ?>
+                                <tr <?php if ($row_count%2) { ?>bgcolor="#F4F4F4"<?php } ?>>
+                                    <td style="padding-left:5px">
+                                        <a href="contact-details.php?id=<?php echo $row_contactlist['donor_id']; ?>"><?php echo $row_contactlist['contact_first']; ?> <?php echo $row_contactlist['contact_last']; ?></a></td>
+                                    <td><?php echo $row_contactlist['receipt_number'] ? $row_contactlist['alloc_short_name'] : $na; ?></td>
+                                    <td><?php echo $row_contactlist['legal_amount'];?></td>
+                                    <td><?php echo $row_contactlist['date_added']; ?></td>
+                                    <td><?php echo $row_contactlist['match_company_name']; ?></td>
+                                    <td><a href="delete.php?donation=<?php echo $row_contactlist['id']; ?>" onclick="javascript:return confirm('Are you sure?')">Delete</a></td>
+                                </tr>
+                            <?php $row_count++; } while ($row_contactlist = mysql_fetch_assoc($contactlist)); ?>
+                        </table>
+                    </form>
+                    <?php include('includes/pagination_contacts.php'); ?>
+                <?php } ?>
+                
+            </div>
+        <?php include('includes/right-column.php'); ?>
+        <br clear="all" />
+        </div>
+        <?php include('includes/footer.php'); ?>
+    </body>
 </html>
