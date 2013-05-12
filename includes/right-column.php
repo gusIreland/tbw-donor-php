2251dca6d7ecd27fc5e710e3fbeb1270720f78c5
Dylan Corwin <dylandcor@gmail.com>
2013-05-12 17:00:55 -0400

    fixed merge conflict and duplicate donation check with associated donors... skipping first donation for some reason

--- includes/right-column.php
+++ includes/right-column.php
@@ -93,24 +93,12 @@ function MM_setTextOfTextfield(objName,x,newText) { //v3.0
 
   <?php if ($pagetitle == 'ContactDetails') { ?>
   <hr />
-<<<<<<< HEAD
   <p><strong>Contact Information</strong><br/><br />
     <?php if ($row_contact['contact_company']) { echo $row_contact['contact_company'] ."<br>"; } ?>
     <?php if ($row_contact['contact_street']) { echo $row_contact['contact_street']  ."<br>"; } ?>
     <?php if ($row_contact['contact_city']) { echo $row_contact['contact_city'] .","; } ?> <?php if ($row_contact['contact_state']) { echo $row_contact['contact_state']; } ?> <?php if ($row_contact['contact_zip']) { echo $row_contact['contact_zip']; } ?><?php if ($row_contact['contact_country']) { echo "<br>".$row_contact['contact_country']; } ?></p>
     <?php if ($row_contact['contact_street'] && $row_contact['contact_city'] && $row_contact['contact_state']) { ?><p><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=<?php echo $row_contact['contact_street']; ?>,+<?php echo $row_contact['contact_city']; ?>,+<?php echo $row_contact['contact_state']; ?>+<?php echo $row_contact['contact_zip']; ?>&gt;" target="_blank">+ View Map </a></p><br />
     <?php } ?>
-    
-=======
-  <p><strong>Contact Information</strong>
-    <?php if ($row_contact['contact_company']) { echo $row_contact['contact_company'] ."<br>"; } ?>
-    <?php if ($row_contact['contact_street']) { echo $row_contact['contact_street']  ."<br>"; } ?>
-    <?php if ($row_contact['contact_city']) { echo $row_contact['contact_city'] .","; } ?> <?php if ($row_contact['contact_state']) { echo $row_contact['contact_state']; } ?> <?php if ($row_contact['contact_zip']) { echo $row_contact['contact_zip']; } ?><?php if ($row_contact['contact_country']) { echo "<br>".$row_contact['contact_country']; } ?></p>
-    <?php if ($row_contact['contact_street'] && $row_contact['contact_city'] && $row_contact['contact_state']) { ?><p><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=<?php echo $row_contact['contact_street']; ?>,+<?php echo $row_contact['contact_city']; ?>,+<?php echo $row_contact['contact_state']; ?>+<?php echo $row_contact['contact_zip']; ?>&gt;" target="_blank">+ View Map </a></p>
-    <br/>
-    <?php } ?>
-
->>>>>>> 9fa497cbb7fdbccf2b3bfd119d1681256f965f80
     <p>      <?php if ($row_contact['contact_phone']) { ?>Phone: <?php echo $row_contact['contact_phone']; ?><br /><?php } ?>
 
       <?php if ($row_contact['contact_web']) { ?>
