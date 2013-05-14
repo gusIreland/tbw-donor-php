755e4e380f2cb36f5351ade12a1ae500835ddd12
Gus Ireland <aireland@andrew.cmu.edu>
2013-05-14 12:08:58 -0400

    fixed

--- includes/header.php
+++ includes/header.php
@@ -39,6 +39,7 @@
       else
       echo "Anonymous"
       ?>
+    </a>
       <?php if ($totalRows_history!=$ih) {?> &middot; <?php } ?>
       <?php $ih++; } while ($row_history = mysql_fetch_assoc($history)); ?></div>
 <?php } ?>
